1351618eecae82497500f2195311e90301eaff28
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-04-01 08:44:42 -0300

    1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
@@ -7,4 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte += 0.1
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+
+
+
 });
