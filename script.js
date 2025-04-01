259fa3f7bb401e4a69d6a7e194ad0149249cc3b0
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-04-01 08:22:40 -0300

    1/04

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentarFonteBotao = document.getElementById('aumentar-fonte');
 
     let tamanhoAtualfonte = 1;
-})
+
+    aumentarFonteBotao.addEventListener('click', function(){
+        tamanhoAtualfonte += 0.1
+        document.body.style.fontsize = `${tamanhoAtualfonte}rem`;
+    });
+});
