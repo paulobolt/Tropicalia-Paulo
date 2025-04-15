989c925ca16c467cabe3b86374e9fa361f55fb4e
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-04-15 08:49:38 -0300

    15

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const botaoDeacessibilidade = document.getElementById('botao-acessibilidade');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
     botaoDeAcessibilidade.addEventListener('click',function(){
