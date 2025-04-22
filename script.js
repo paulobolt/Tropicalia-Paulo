15489ffd6a2f939a7e1db51998096d904a5e9985
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-04-22 08:18:56 -0300

    24

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const AlternaContraste = document.getElementById('alterna-contraste');
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -24,4 +26,9 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-});
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+        })
+
+    });
