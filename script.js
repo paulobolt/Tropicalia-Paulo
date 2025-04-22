32d3565a0da12b60990eebaa9ffb17b4b09a1bc6
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-04-22 08:20:10 -0300

    67

--- script.js
+++ script.js
@@ -30,5 +30,4 @@ document.addEventListener('DOMContentLoaded', function () {
     alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste');
         })
-
     });
